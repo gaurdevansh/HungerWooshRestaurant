a1a38f4dfd12d020d1f3f6ed4ad93e9ed11f5211
Devansh <devanshgaur75@gmail.com>
2024-01-15 23:42:07 +0530

    Added Restaurant User Signup using Firebase

--- app/src/main/java/com/example/hungerwooshrestaurant/SignUpActivity.kt
+++ app/src/main/java/com/example/hungerwooshrestaurant/SignUpActivity.kt
@@ -3,12 +3,27 @@ package com.example.hungerwooshrestaurant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.hungerwooshrestaurant.databinding.ActivitySignUpBinding
+import com.example.hungerwooshrestaurant.model.RestaurantUser
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import kotlin.math.log
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var username: String
+    private lateinit var restaurantName: String
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +31,48 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
         setUpLocationChooser()
 
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
         binding.tvAlreadyHaveAccount.setOnClickListener {
             finish()
         }
         binding.btnCreateAccount.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            checkInputFields()
         }
+    }
+
+    private fun checkInputFields() {
+        username = binding.etOwner.text.toString().trim()
+        restaurantName = binding.etRestaurant.text.toString().trim()
+        email = binding.etEmail.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
+
+        if(username.isBlank() || restaurantName.isBlank() || email.isBlank() || password.isBlank()) {
+            Toast.makeText(this, "Please fill all details", Toast.LENGTH_SHORT).show()
+        } else {
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                Toast.makeText(this, "Account Created Successfully", Toast.LENGTH_SHORT).show()
+                saveUserData()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Account Creation Failed", Toast.LENGTH_SHORT).show()
+                Log.d("Account", "createAccount: Failure", task.exception)
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        val user = RestaurantUser(username, restaurantName, email, password)
+        val userId: String = FirebaseAuth.getInstance().currentUser!!.uid
+        database.child("user").child(userId).setValue(user)
     }
 
     private fun setUpLocationChooser() {
---
+++ app/src/main/java/com/example/hungerwooshrestaurant/model/RestaurantUser.kt
@@ -0,0 +1,8 @@
+package com.example.hungerwooshrestaurant.model
+
+data class RestaurantUser(
+    val username: String? = null,
+    val restaurantName: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+)
